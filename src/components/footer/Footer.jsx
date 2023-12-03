d475b85b5c59d6806dc749752a787ad8d9ecb16c
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-04 01:04:45 +0530

    navbar and footer completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <div>@2023 abhishekkumar. All rights reserved!</div>
-        <div></div>
+        <div>icons</div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import styles from './navbar.module.css'
 
 const links = [
   { id: 1, title: "Home", url: "/" },
@@ -12,14 +13,17 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href={"/"}>Logo</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href={"/"} className={styles.logo}>Logo</Link>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
+        <button className={styles.logout}>
+          Logout
+        </button>
       </div>
     </div>
   );
