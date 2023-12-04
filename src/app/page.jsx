0635417199e5eab08f6640d0ac3fa42c44b245a2
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-04 11:44:25 +0530

    Homepage ui design completed and add socials icons in footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import hero from "public//heroImage.png";
+import styles from "./page.module.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Lorem ipsum dolor sit amet consectetur .{" "}
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur.
+          temporibus rerum labore! Quasi corrupti saepe architecto quod minima nulla, inventore aliquam iusto, .
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={hero} alt="Home Image" className={styles.image} />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
         <div>@2023 abhishekkumar. All rights reserved!</div>
-        <div>icons</div>
+        <div className={styles.social}>
+          <Image className={styles.icons} src='/fbLogo.png' width={20} height={20} alt='Facebook' />
+          <Image className={styles.icons} src='/linkedin.png' width={20} height={20} alt='LinkedIn' />
+          <Image className={styles.icons} src='/GithubLogo.png' width={20} height={20} alt='GitHub' />
+        </div>
     </div>
   )
 }
